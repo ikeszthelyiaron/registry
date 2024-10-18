45de0d2c69093f663f25e1f248211dd753c227bb
Aron Keszthelyi <ikeszthelyiaron@gmail.com>
2024-10-18 03:37:58 +0200

    implement tests for happy paths in ContactDetailsServiceImpl

--- src/test/java/hu/otp/assignment/ContactDetailServiceImplTests.java
+++ src/test/java/hu/otp/assignment/ContactDetailServiceImplTests.java
@@ -19,9 +19,9 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 import static hu.otp.assignment.domain.Platform.SKYPE;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import static hu.otp.assignment.domain.Platform.TWITTER;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -76,12 +76,11 @@ public class ContactDetailServiceImplTests {
         ContactDetailDto contactDetailDto = new ContactDetailDto(SKYPE, "ABC", 1);
         Person person = mock(Person.class);
         ContactDetail contactDetail = mock(ContactDetail.class);
-        person.setContactDetails(new ArrayList<ContactDetail>());
         when(contactDetailRepository.findById(1L)).thenReturn(Optional.of(contactDetail));
         when(personRepository.findById(1L)).thenReturn(Optional.of(person));
         when(person.hasSuchPlatform(contactDetailDto.platform())).thenReturn(true);
         when(person.findByPlatform(contactDetailDto.platform())).thenReturn(contactDetail);
-        when(person.findByPlatform(contactDetailDto.platform()).getId()).thenReturn(5L);
+        when(contactDetail.getId()).thenReturn(5L);
         assertThrows(AlreadyPresentOnPlatformException.class,
                 () -> contactDetailServiceImpl.editContactDetail(contactDetailDto, 1));
     }
@@ -140,6 +139,77 @@ public class ContactDetailServiceImplTests {
     void givenNoPerson_WhenGetContactDetailsByPersonId_ThenExceptionThrown() throws Exception {
         assertThrows(NoPersonWithSuchIdException.class,
                 () -> contactDetailServiceImpl.getContactDetailsByPersonId(1));
+    }
+
+    @Test
+    void givenPersonWithContactDetail_WhenDeleteContactDetail_ThenContactDetailDeleted() throws Exception {
+        Person person = new Person();
+        person.setContactDetails(new ArrayList<ContactDetail>());
+        ContactDetail contactDetail = new ContactDetail();
+        contactDetail.setPerson(person);
+        contactDetail.setPlatform(SKYPE);
+        contactDetail.setIdentifier("abc");
+        person.addContactDetail(contactDetail);
+        PlatformDto platformDto = new PlatformDto(SKYPE);
+        when(personRepository.findById(1L)).thenReturn(Optional.of(person));
+        when(contactDetailRepository.findByPersonIdAndPlatform(1, SKYPE)).thenReturn(Optional.of(contactDetail));
+
+        contactDetailServiceImpl.deleteContactDetail(1, platformDto);
+        assertTrue(person.getContactDetails().isEmpty());
+        assertTrue(contactDetailRepository.findAll().isEmpty());
+    }
+
+    @Test
+    void givenPersonWithContactDetail_WhenEditContactDetail_ThenContactDetailEdited() throws Exception {
+        Person person = new Person();
+        person.setContactDetails(new ArrayList<ContactDetail>());
+        ContactDetail contactDetail = new ContactDetail();
+        contactDetail.setId(1L);
+        contactDetail.setPerson(person);
+        contactDetail.setPlatform(SKYPE);
+        contactDetail.setIdentifier("abc");
+        person.addContactDetail(contactDetail);
+        ContactDetailDto contactDetailDto = new ContactDetailDto(SKYPE, "def", 1L);
+        when(personRepository.findById(1L)).thenReturn(Optional.of(person));
+        when(contactDetailRepository.findById(1L)).thenReturn(Optional.of(contactDetail));
+        contactDetailServiceImpl.editContactDetail(contactDetailDto, 1);
+        assertEquals(1, person.getContactDetails().size());
+        assertEquals("def", person.getContactDetails().get(0).getIdentifier());
+        assertEquals(SKYPE, person.getContactDetails().get(0).getPlatform());
+    }
+
+    @Test
+    void givenPersonAndContactDetail_WhenAddContactDetailToPerson_ThenContactDetailAdded() throws Exception {
+        Person person = new Person();
+        person.setContactDetails(new ArrayList<ContactDetail>());
+        when(personRepository.findById(1L)).thenReturn(Optional.of(person));
+        ContactDetailDto contactDetailDto = new ContactDetailDto(SKYPE, "def", 1L);
+        ContactDetail contactDetail = new ContactDetail();
+        contactDetail.setPlatform(SKYPE);
+        contactDetail.setIdentifier("def");
+        when(contactDetailMapper.dtoToEntity(contactDetailDto)).thenReturn(contactDetail);
+
+        contactDetailServiceImpl.addContactDetailToPerson(contactDetailDto);
+        assertEquals(1, person.getContactDetails().size());
+        assertEquals("def", person.getContactDetails().get(0).getIdentifier());
+        assertEquals(SKYPE, person.getContactDetails().get(0).getPlatform());
+    }
+
+    @Test
+    void WhenPersonWithContactDetail_WhenGetContactDetailsByPersonId_ThenCorrectListReturned () throws Exception {
+        Person person = new Person();
+        person.setContactDetails(new ArrayList<ContactDetail>());
+        when(personRepository.findById(1L)).thenReturn(Optional.of(person));
+        ContactDetailDto contactDetailDto = new ContactDetailDto(SKYPE, "def", 1L);
+        ContactDetail contactDetail = new ContactDetail();
+        contactDetail.setPlatform(SKYPE);
+        contactDetail.setIdentifier("def");
+        when(contactDetailMapper.dtoToEntity(contactDetailDto)).thenReturn(contactDetail);
+        contactDetailServiceImpl.addContactDetailToPerson(contactDetailDto);
+        contactDetailServiceImpl.getContactDetailsByPersonId(1);
+        assertEquals(1, person.getContactDetails().size());
+        assertEquals("def", person.getContactDetails().get(0).getIdentifier());
+        assertEquals(SKYPE, person.getContactDetails().get(0).getPlatform());
     }
 
 
